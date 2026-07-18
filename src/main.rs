69a79db39b1e99febaa28af857e0b7827344bf23
Ross MacArthur <ross@macarthur.io>
2026-07-18 23:39:41 +0200

    Feed stdin to `jq` in a separate thread
    This allows us to correctly handle cases where `jq` exits early without
    reading stdin.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use std::io;
 use std::io::prelude::*;
 use std::io::IsTerminal;
 use std::path::PathBuf;
+use std::process;
 use std::process::{ChildStdin, ChildStdout, Command, Stdio};
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
 
 use anyhow::{Context, Result};
 use serde_json as json;
@@ -22,54 +27,81 @@ pub struct Transcoder {
 }
 
 fn main() -> Result<()> {
-    let t = parse::args()?;
+    let tc = Arc::new(parse::args()?);
 
     let mut cmd = Command::new("jq");
 
-    if !t.opt.info.filter && io::stdin().is_terminal() {
+    if !tc.opt.info.filter && io::stdin().is_terminal() {
         parse::usage();
     }
 
-    if t.opt.force_color_output && io::stdout().is_terminal() {
+    if tc.opt.force_color_output && io::stdout().is_terminal() {
         cmd.arg("-C");
     }
 
-    cmd.args(&t.opt.args);
-    cmd.stdin(Stdio::piped());
+    cmd.args(&tc.opt.args);
     cmd.stdin(Stdio::piped());
     cmd.stdout(Stdio::piped());
     cmd.stderr(Stdio::inherit());
 
-    // Spawn `jq` and transcode input and output
     let mut jq = cmd.spawn()?;
 
-    // NB! `stdin` must be dropped otherwise `jq` will never exit
-    if !t.opt.info.null_input {
-        let mut stdin = jq.stdin.take().unwrap();
-        if t.opt.files.is_empty() {
-            t.transcode_input(io::stdin(), &mut stdin)?;
-        } else {
-            for path in &t.opt.files {
-                if path.to_str() == Some("-") {
-                    t.transcode_input(io::stdin(), &mut stdin)?;
-                } else {
-                    let file = File::open(path).with_context(|| {
-                        format!("failed to open `{}`", PathBuf::from(path).display())
-                    })?;
-                    t.transcode_input(file, &mut stdin)?;
-                }
+    // Feed input to `jq` in a separate thread
+    let rx = if tc.opt.info.null_input {
+        None
+    } else {
+        let (tx, rx) = mpsc::channel();
+        let tc = tc.clone();
+        let stdin = jq.stdin.take().expect("piped");
+        thread::spawn(move || {
+            if let Err(err) = tx.send(tc.feed_input(stdin)) {
+                panic!("aq: failed to send result from thread: {}", err);
             }
+        });
+        Some(rx)
+    };
+
+    let stdout = jq.stdout.take().expect("piped");
+    tc.feed_output(stdout)?;
+
+    // Now wait for `jq` to exit
+    let status = jq.wait()?;
+
+    // Exit with the same exit code as `jq`
+    if !status.success() {
+        process::exit(status.code().unwrap_or(1));
+    }
+
+    // Wait for the input thread to finish and check for errors
+    if let Some(rx) = rx {
+        if let Ok(result) = rx.recv_timeout(Duration::from_millis(100)) {
+            result?;
         }
     }
 
-    let mut stdout = jq.stdout.take().unwrap();
-    t.transcode_output(&mut stdout, io::stdout())?;
-
-    jq.wait()?;
     Ok(())
 }
 
 impl Transcoder {
+    fn feed_input(&self, mut jq: ChildStdin) -> Result<()> {
+        let jq = &mut jq;
+        if self.opt.files.is_empty() {
+            self.transcode_input(io::stdin(), jq)
+        } else {
+            for path in &self.opt.files {
+                if path.to_str() == Some("-") {
+                    self.transcode_input(io::stdin(), jq)?;
+                } else {
+                    let file = File::open(path).with_context(|| {
+                        format!("failed to open `{}`", PathBuf::from(path).display())
+                    })?;
+                    self.transcode_input(file, jq)?;
+                }
+            }
+            Ok(())
+        }
+    }
+
     fn transcode_input<R: Read>(&self, mut input: R, jq: &mut ChildStdin) -> Result<()> {
         match self.input {
             Format::Json => {
@@ -89,6 +121,11 @@ impl Transcoder {
                 transcode(de, &mut ser).context("failed to transcode from YAML to JSON")?
             }
         }
+        Ok(())
+    }
+
+    fn feed_output(&self, mut jq: ChildStdout) -> Result<()> {
+        self.transcode_output(&mut jq, io::stdout())?;
         Ok(())
     }
 
